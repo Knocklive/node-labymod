0a84a004f70870f03e9b77fe70206f2ec53adac5
MasterJanic <dorfeind@gmail.com>
2020-12-18 18:20:34 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-let getStickers = require('./methods/getStickers')
-getStickers('32645503-0d51-4fc6-8e4a-28b03714db5e').then(s => console.log(s))
-
 module.exports = {
   getStatus: require('./methods/getStatus'),
   getShop: require('./methods/getShop'),
