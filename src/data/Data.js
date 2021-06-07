43829592c652ef37a57af03b4722de11e15d1a09
masterjanic <68118279+masterjanic@users.noreply.github.com>
2021-06-07 17:24:37 +0200

    refactor: Reduce code

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,16 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
-const availableRoles = JSON.parse(fs.readFileSync(path.join(__dirname, './roles.json'), 'utf8'));
-const availableEmotes = JSON.parse(fs.readFileSync(path.join(__dirname, './emotes.json'), 'utf8'));
-const availableStickers = JSON.parse(fs.readFileSync(path.join(__dirname, './stickers.json'), 'utf8'));
-const availableCosmetics = JSON.parse(fs.readFileSync(path.join(__dirname, './cosmetics.json'), 'utf8'));
+const { readFileSync } = require('fs');
+const { join } = require('path');
 
 module.exports = {
-  availableRoles,
-  availableEmotes,
-  availableStickers,
-  availableCosmetics,
+  availableRoles: JSON.parse(readFileSync(join(__dirname, './roles.json'), 'utf8')),
+  availableEmotes: JSON.parse(readFileSync(join(__dirname, './emotes.json'), 'utf8')),
+  availableStickers: JSON.parse(readFileSync(join(__dirname, './stickers.json'), 'utf8')),
+  availableCosmetics: JSON.parse(readFileSync(join(__dirname, './cosmetics.json'), 'utf8')),
 };
